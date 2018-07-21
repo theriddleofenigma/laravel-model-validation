54b58de1ed28e3b67e5d3c75315bca9bc3b13f06
= <kumaravel@mallow-tech.com>
2018-07-21 17:58:56 +0530

    Updated the package name.

--- src/ModelValidator.php
+++ src/ModelValidator.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Packages;
+namespace Paradox;
 
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Validator;
 
 /**
  * Class ModelValidator
--- src/ValidatorTrait.php
+++ src/ValidatorTrait.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Packages;
+namespace Paradox;
 
 trait ValidatorTrait
 {

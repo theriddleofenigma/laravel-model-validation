a041bc920b036dbe04ea1c2bdb423829051be2a9
Kumaravel <kumarwindows11@gmail.com>
2018-07-24 10:19:16 +0530

    Added comments and alias method.

--- src/ValidatorTrait.php
+++ src/ValidatorTrait.php
@@ -1,9 +1,13 @@
 <?php
 
-namespace Paradox;
+namespace CodeDevPal;
 
 trait ValidatorTrait
 {
+    /**
+     * Calls the validator instance to validate
+     * Also runs the beforeValidation and afterValidation methods if exists
+     */
     public function validate()
     {
         // Runs the logic that might be executed before the model validation
@@ -14,5 +18,14 @@ trait ValidatorTrait
 
         // Runs the logic that might be executed after the model validation
         if (method_exists($this, 'afterValidation')) $this->afterValidation();
+    }
+    
+    /**
+     * Alias method for calling the validate method on saving in boot
+     */
+    public function validateOnSaving() {
+        static::saving(function($model){
+            $model->validate();
+        });
     }
 }
